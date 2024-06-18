47f9637ec95a854e19e7fee8de8364732bc4414f
caio <caio2002araujo@gmail.com>
2024-06-18 14:21:59 -0300

    feat(adding config husky): adding config husky
    husky
    
    BREAKING CHANGE: adding config husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BarcodeFlow",
-  description: "Site para geração de barcode",
+  description: "Site para geração de barcodess",
 };
 
 export default function RootLayout({

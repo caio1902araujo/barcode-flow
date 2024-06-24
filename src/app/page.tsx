7969547bf5989734b837b31b0b4288ca4da901cf
Breno <brenomoraes97@yahoo.com>
2024-06-24 01:33:22 -0300

    feat(toast): created toast component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import { Input } from "~/components/ui/input.tsx";
+"use client";
+import { Button } from "~/components/ui/button";
+import { useToast } from "~/components/ui/toast/use-toast";
 
 export default function Home() {
+  const { toast } = useToast();
   return (
     <main>
       <p>Geração de barcode</p>
+      <Button
+        onClick={() => {
+          toast({
+            title: "aaaa",
+            description: "aaaa",
+          });
+        }}
+      >
+        Show Toast
+      </Button>
     </main>
   );
 }
